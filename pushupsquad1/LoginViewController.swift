772cc00d182c619b28d023a15fdddb7aa7ba4f86
bigmattie <matthewmanning2014@gmail.com>
2020-05-28 20:09:35 -0700

    added v1 of aapl login

--- pushupsquad1/LoginViewController.swift
+++ pushupsquad1/LoginViewController.swift
@@ -9,10 +9,40 @@
 import UIKit
 import FBSDKLoginKit
 import Firebase
+import AuthenticationServices //APPLE LOGIN
+import CryptoKit
 
 class LoginViewController: UIViewController {
     
     var handle: AuthStateDidChangeListenerHandle?
+    // Unhashed nonce.
+    fileprivate var currentNonce: String?
+
+    @available(iOS 13, *)
+    func startSignInWithAppleFlow() {
+      let nonce = randomNonceString()
+      currentNonce = nonce
+      let appleIDProvider = ASAuthorizationAppleIDProvider()
+      let request = appleIDProvider.createRequest()
+      request.requestedScopes = [.fullName, .email]
+      request.nonce = sha256(nonce)
+
+      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+      authorizationController.delegate = self
+      authorizationController.presentationContextProvider = self
+      authorizationController.performRequests()
+    }
+
+    @available(iOS 13, *)
+    private func sha256(_ input: String) -> String {
+      let inputData = Data(input.utf8)
+      let hashedData = SHA256.hash(data: inputData)
+      let hashString = hashedData.compactMap {
+        return String(format: "%02x", $0)
+      }.joined()
+
+      return hashString
+    }
     
     @IBOutlet weak var facebookLoginButton: UIButton!
     @IBOutlet weak var appleLoginButton: UIButton!
@@ -37,8 +67,61 @@ class LoginViewController: UIViewController {
                     self.finishLogin()
                  }
             }
+        performExistingAccountSetupFlows()
+        
+        
+    }
+    //
+    //AAPL Sign in
+    //
+    /// - Tag: add_appleid_button
+    func setupProviderLoginView() {
+//        let authorizationButton = ASAuthorizationAppleIDButton()
+        appleLoginButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+//        self.loginProviderStackView.addArrangedSubview(authorizationButton)
     }
     
+    // - Tag: perform_appleid_password_request
+    /// Prompts the user if an existing iCloud Keychain credential or Apple ID credential is found.
+    func performExistingAccountSetupFlows() {
+        // Prepare requests for both Apple ID and password providers.
+//        let requests = [ASAuthorizationAppleIDProvider().createRequest(),
+//                        ASAuthorizationPasswordProvider().createRequest()]
+//        
+//        // Create an authorization controller with the given requests.
+//        let authorizationController = ASAuthorizationController(authorizationRequests: requests)
+//        authorizationController.delegate = self
+//        authorizationController.presentationContextProvider = self
+//        authorizationController.performRequests()
+    }
+    
+    /// - Tag: perform_appleid_request
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+//        let appleIDProvider = ASAuthorizationAppleIDProvider()
+//        let request = appleIDProvider.createRequest()
+//        request.requestedScopes = [.fullName, .email]
+        
+        let nonce = randomNonceString()
+        currentNonce = nonce
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        request.nonce = sha256(nonce)
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+        
+        //firebase sign in logic
+
+
+    }
+    
+    //
+    //ANON SIGN IN
+    //
     func anonSignIn(_ sender: Any) {
        Auth.auth().signInAnonymously() { (authResult, error) in
          // ...
@@ -104,6 +187,7 @@ class LoginViewController: UIViewController {
     
     @IBAction func appleLogin(_ sender: Any) {
         print("Apple Login")
+        handleAuthorizationAppleIDButtonPress()
     }
     
     
@@ -118,3 +202,179 @@ class LoginViewController: UIViewController {
     */
 
 }
+
+@available(iOS 13.0, *)
+extension LoginViewController: ASAuthorizationControllerDelegate {
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+    if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+      guard let nonce = currentNonce else {
+        fatalError("Invalid state: A login callback was received, but no login request was sent.")
+      }
+      guard let appleIDToken = appleIDCredential.identityToken else {
+        print("Unable to fetch identity token")
+        return
+      }
+      guard let idTokenString = String(data: appleIDToken, encoding: .utf8) else {
+        print("Unable to serialize token string from data: \(appleIDToken.debugDescription)")
+        return
+      }
+      // Initialize a Firebase credential.
+      let credential = OAuthProvider.credential(withProviderID: "apple.com",
+                                                idToken: idTokenString,
+                                                rawNonce: nonce)
+      // Sign in with Firebase.
+      Auth.auth().signIn(with: credential) { (authResult, error) in
+        if (error != nil) {
+          // Error. If error.code == .MissingOrInvalidNonce, make sure
+          // you're sending the SHA256-hashed nonce as a hex string with
+          // your request to Apple.
+          print(error?.localizedDescription)
+          return
+        }
+        self.finishLogin()
+        // User is signed in to Firebase with Apple.
+        // ...
+      }
+    }
+  }
+
+  func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+    // Handle error.
+    print("Sign in with Apple errored: \(error)")
+  }
+
+}
+//extension LoginViewController: ASAuthorizationControllerDelegate {
+//    /// - Tag: did_complete_authorization
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+//        switch authorization.credential {
+//        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+//
+//            // Create an account in your system.
+//            let userIdentifier = appleIDCredential.user
+//            let fullName = appleIDCredential.fullName
+//            let email = appleIDCredential.email
+//
+//            // For the purpose of this demo app, store the `userIdentifier` in the keychain.
+//            self.saveUserInKeychain(userIdentifier)
+//
+//            // For the purpose of this demo app, show the Apple ID credential information in the `ResultViewController`.
+////            self.showResultViewController(userIdentifier: userIdentifier, fullName: fullName, email: email)
+//
+//        case let passwordCredential as ASPasswordCredential:
+//
+//            // Sign in using an existing iCloud Keychain credential.
+//            let username = passwordCredential.user
+//            let password = passwordCredential.password
+//
+//            // For the purpose of this demo app, show the password credential as an alert.
+//            DispatchQueue.main.async {
+//                self.showPasswordCredentialAlert(username: username, password: password)
+//            }
+//
+//
+//        default:
+//            break
+//        }
+//    }
+//
+//    private func saveUserInKeychain(_ userIdentifier: String) {
+//        do {
+//            try KeychainItem(service: "com.example.apple-samplecode.juice", account: "userIdentifier").saveItem(userIdentifier)
+//        } catch {
+//            print("Unable to save userIdentifier to keychain.")
+//        }
+//    }
+//
+////    private func showResultViewController(userIdentifier: String, fullName: PersonNameComponents?, email: String?) {
+////        guard let viewController = self.presentingViewController as? ResultViewController
+////            else { return }
+////
+////        DispatchQueue.main.async {
+////            viewController.userIdentifierLabel.text = userIdentifier
+////            if let givenName = fullName?.givenName {
+////                viewController.givenNameLabel.text = givenName
+////            }
+////            if let familyName = fullName?.familyName {
+////                viewController.familyNameLabel.text = familyName
+////            }
+////            if let email = email {
+////                viewController.emailLabel.text = email
+////            }
+////            self.dismiss(animated: true, completion: nil)
+////        }
+////    }
+//
+//    private func showPasswordCredentialAlert(username: String, password: String) {
+//        let message = "The app has received your selected credential from the keychain. \n\n Username: \(username)\n Password: \(password)"
+//        let alertController = UIAlertController(title: "Keychain Credential Received",
+//                                                message: message,
+//                                                preferredStyle: .alert)
+//        alertController.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
+//        self.present(alertController, animated: true, completion: nil)
+//    }
+//
+//    /// - Tag: did_complete_error
+//    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+//        // Handle error.
+//    }
+//}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    /// - Tag: provide_presentation_anchor
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
+extension UIViewController {
+    
+    func showLoginViewController() {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        if let loginViewController = storyboard.instantiateViewController(withIdentifier: "loginViewController") as? LoginViewController {
+            loginViewController.modalPresentationStyle = .formSheet
+            loginViewController.isModalInPresentation = true
+            self.present(loginViewController, animated: true, completion: nil)
+        }
+    }
+}
+
+
+//Firebase apple logic
+
+// Adapted from https://auth0.com/docs/api-auth/tutorials/nonce#generate-a-cryptographically-random-nonce
+private func randomNonceString(length: Int = 32) -> String {
+  precondition(length > 0)
+  let charset: Array<Character> =
+      Array("0123456789ABCDEFGHIJKLMNOPQRSTUVXYZabcdefghijklmnopqrstuvwxyz-._")
+  var result = ""
+  var remainingLength = length
+
+  while remainingLength > 0 {
+    let randoms: [UInt8] = (0 ..< 16).map { _ in
+      var random: UInt8 = 0
+      let errorCode = SecRandomCopyBytes(kSecRandomDefault, 1, &random)
+      if errorCode != errSecSuccess {
+        fatalError("Unable to generate nonce. SecRandomCopyBytes failed with OSStatus \(errorCode)")
+      }
+      return random
+    }
+
+    randoms.forEach { random in
+      if remainingLength == 0 {
+        return
+      }
+
+      if random < charset.count {
+        result.append(charset[Int(random)])
+        remainingLength -= 1
+      }
+    }
+  }
+
+  return result
+}
+
+
+
